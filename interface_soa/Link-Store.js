952b35595740f652edf96fe1ee5fbadd6fad5938
nnewnew <godsnew2542@gmail.com>
2019-11-24 18:06:41 +0700

    ...

--- interface_soa/Link-Store.js
+++ interface_soa/Link-Store.js
@@ -1,28 +1,49 @@
 // Charts-list.html
-var Charts_list = "http://127.0.0.1:8080/playstore";
+var Charts_list = "11";
 // Free-list.html
-var Free_list = "";
+var Free_list = "11";
+// Price-list.html
+var Price_list = "11";
+// All-list.html
+var All_list = "http://127.0.0.1:8080/playstore";
+// Top-Free.html
+var Top_Free = "11";
+// Top-price.html
+var Top_Price = "11";
+// Rating-5.html
+var Rating5 = "11";
+// Rating-4.html
+var Rating4 = "11";
+// Rating-3.html
+var Rating3 = "11";
+// Rating-2.html
+var Rating2 = "11";
+// Rating-1.html
+var Rating1 = "11";
+// Top-10.html
+var Top10 = "11";
 
 
-$(document).ready(function() {
-  $.get(Free_list, function(responseData) {
-    $("#Free-table").DataTable({
-      processing: true,
-      data: responseData,
-        columns: [
-          { 
-            "data": "app",
-            "render": function(data, type, row, meta){
-               if(type === 'display'){
-                   data = '<a href="index.html"' + data + '">' + data + '</a>';
-               }   
-               return data;
-            }
-         },
-          { data: "rating" },
-          { data: "installs" },
-          { data: "price" },         
-        ]    
-    });
-  });
-});
+
+// $(document).ready(function() {
+//   $.get(Free_list, function(responseData) {
+//     $("#Free-table").DataTable({
+//       processing: true,
+//       data: responseData,
+//         columns: [
+//           { 
+//             "data": "app",
+//             "render": function(data, type, row, meta){
+//                if(type === 'display'){
+//                    data = '<a href="index.html"' + data + '">' + data + '</a>';
+//                }   
+//                return data;
+//             }
+//          },
+//           { data: "rating" },
+//           { data: "installs" },
+//           { data: "price" },         
+//         ]    
+//     });
+//   });
+// });
